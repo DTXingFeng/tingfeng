d34d6fb205af09d5deedcc6b8471edfe1f956a18
DTXingFeng <2695570953@qq.com>
2025-12-03 12:20:26 +0800

    first commit

---
+++ src/main/java/xyz/xingfeng/tingfeng/TingfengApplication.java
@@ -0,0 +1,13 @@
+package xyz.xingfeng.tingfeng;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TingfengApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TingfengApplication.class, args);
+	}
+
+}
